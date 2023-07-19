b130b9cac34db9da85d927c0adb44e7be3aba2d1
devdrx <68637772+devdrx@users.noreply.github.com>
2023-07-19 22:33:26 +0530

    Create Simulation.cpp

---
+++ Simulation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+#define vi vector<int>
+#define ll long long
+int main() {
+	{
+#ifndef ONLINE_JUDGE
+		// for getting input from input.txt
+		freopen("input.txt", "r", stdin);
+		// for writing output to output.txt
+		freopen("output.txt", "w", stdout);
+#endif
+	}
+	vi counter(3);
+	vi  shellpos(3);
+	for (int i = 0; i < 3; i++) {
+		shellpos[i	] = i;
+	}
+	int t, a, b, g;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b >> g;
+		a--; b--; g--;
+		swap(shellpos[a], shellpos[b]);
+		counter[shellpos[g]]++;   //why is this so hard to understand
+ 	}
+	cout << max({counter[0], counter[1], counter[2]});
+	return 0;
+}
+
+//arr[j][k]
+// 1 2 3     2 1 3 (1 2 swap)	if it was at 2 and she guesses  index 1 then its correct; shellpos[1] =  2 ; counter[2] = 1
